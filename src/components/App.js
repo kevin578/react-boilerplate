9ab00d382f92a78753d23f9e9cab7d1d968c2037
kevin578 <kbriggs@aapl000894.local>
2017-12-18 16:33:24 -0500

    added eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 export default class App extends React.Component {
-    render(){
-        return (
-            <div className = "app">
-            <h1>Delete me and get stated...</h1>
-            </div>
-        )
-    }
+
+  
+
+    render() {
+    return (
+      <div className="app">
+        
+      </div>
+    );
+  }
 }
