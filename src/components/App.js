e558747b2d71be8c79d3e9f72428d195454d1bd3
kevin578 <kbriggs@aapl000894.local>
2017-12-18 16:11:55 -0500

    initial boilerplate set up

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render(){
+        return (
+            <div className = "app">
+            <h1>Delete me and get stated...</h1>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'normalize.css/normalize.css';
 import './index.scss';
+import 'normalize.css/normalize.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
